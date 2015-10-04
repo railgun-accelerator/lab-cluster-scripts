cfc4f934326d4e3195b00379a8a5e213f849b60c
Haowen Xu <public@korepwx.com>
2015-10-04 17:40:54 +0800

    minor update

--- pxeboot/copy-to-dist.sh
+++ pxeboot/copy-to-dist.sh
@@ -1,42 +1,5 @@
 #!/bin/bash
 
-# Setup syslinux boot loader
-(
-rm -f /srv/tftp/pxelinux.cfg
-rm -f /srv/tftp/pxelinux.0
-
-cd /tmp
-wget https://www.kernel.org/pub/linux/utils/boot/syslinux/syslinux-6.03.tar.gz
-tar -xvf syslinux-6.03.tar.gz
-
-mkdir -p /srv/tftp/bios
-cp syslinux-6.03/bios/core/pxelinux.0 /srv/tftp/bios/
-cp syslinux-6.03/bios/com32/elflink/ldlinux/ldlinux.c32 /srv/tftp/bios/
-cp syslinux-6.03/bios/com32/lib/libcom32.c32 /srv/tftp/bios/
-cp syslinux-6.03/bios/com32/libutil/libutil.c32 /srv/tftp/bios/
-cp syslinux-6.03/bios/com32/menu/vesamenu.c32 /srv/tftp/bios/
-cp syslinux-6.03/bios/com32/modules/pxechn.c32 /srv/tftp/bios/
-
-mkdir -p /srv/tftp/efi64
-cp syslinux-6.03/efi64/efi/syslinux.efi /srv/tftp/efi64/
-cp syslinux-6.03/efi64/com32/elflink/ldlinux/ldlinux.e64 /srv/tftp/efi64/
-cp syslinux-6.03/efi64/com32/lib/libcom32.c32 /srv/tftp/efi64/
-cp syslinux-6.03/efi64/com32/libutil/libutil.c32 /srv/tftp/efi64/
-cp syslinux-6.03/efi64/com32/menu/vesamenu.c32 /srv/tftp/efi64/
-cp syslinux-6.03/efi64/com32/modules/pxechn.c32 /srv/tftp/efi64/
-
-mkdir -p /srv/tftp/efi32
-cp syslinux-6.03/efi32/efi/syslinux.efi /srv/tftp/efi32/
-cp syslinux-6.03/efi32/com32/elflink/ldlinux/ldlinux.e32 /srv/tftp/efi32/
-cp syslinux-6.03/efi32/com32/lib/libcom32.c32 /srv/tftp/efi32/
-cp syslinux-6.03/efi32/com32/libutil/libutil.c32 /srv/tftp/efi32/
-cp syslinux-6.03/efi32/com32/menu/vesamenu.c32 /srv/tftp/efi32/
-cp syslinux-6.03/efi32/com32/modules/pxechn.c32 /srv/tftp/efi32/
-
-rm -rf syslinux-6.03
-rm -f syslinux-6.03.tar.gz
-)
-
 # copy boot menu to each boot loader
 rsync -avP pxelinux.cfg/ /srv/tftp/bios/pxelinux.cfg
 rsync -avP pxelinux.cfg/ /srv/tftp/efi32/pxelinux.cfg
