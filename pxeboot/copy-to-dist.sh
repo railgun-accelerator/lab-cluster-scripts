fcd8a0e4e11d13e8dd2d1dfc8c6968bf711f3f04
Haowen Xu <public@korepwx.com>
2015-10-04 17:36:57 +0800

    minor update

--- pxeboot/copy-to-dist.sh
+++ pxeboot/copy-to-dist.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 rm -f /srv/tftp/pxelinux.cfg
-rsync -avP pxelinux.cfg/ /srv/tftp/pxelinux.cfg
+rsync -avP pxelinux.cfg/ /srv/tftp/bios/pxelinux.cfg
+rsync -avP pxelinux.cfg/ /srv/tftp/efi32/pxelinux.cfg
+rsync -avP pxelinux.cfg/ /srv/tftp/efi64/pxelinux.cfg
