6c6773e5ff66a9d9e3186b7dd4a129384552225c
Korepwx Xu <public@korepwx.com>
2015-10-21 22:28:24 +0800

    minor fix

--- routes/mkroutes.sh
+++ routes/mkroutes.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 (python gfwtools.py networks vpn --prefer include --base foreign --excludes data/thunets.txt,data/cnnets.txt | python gfwtools.py generate vpnroutes --gateway 10.122.0.2) > /usr/local/bin/vpnroutes.sh
+chmod +x /usr/local/bin/vpnroutes.sh
 rm -f data/delegated-apnic-latest
